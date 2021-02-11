b42ee37ca26fd99b46ece079c07d9cd682824e63
nada <na1351@fayoum.edu.eg>
2021-02-11 18:11:27 +0200

    commit project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TodoItem from './components/TodoItem/todoitem';
+import AddItem from './components/AddItem/additem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    items: [
+      {id:1, name:"Nada", age:24},
+      {id:2, name:"Manar", age:21},
+      {id:3, name:"Mohamed", age:15}
+    ]
+  }
+
+  deleteItem =(id) =>{
+    // console.log(id);
+    // way one to delete item from array and update array using findIndex
+    // let items = this.state.items;
+    // let i =  items.findIndex(item=> item.id === id)
+    // items.splice(i,1)
+    // this.setState({items:items})  // or this.setState({items})
+        
+                    //<---------------------------->
+
+    // way two to delete item from array and update array using fillter  
+    let items = this.state.items.filter(item => {
+      return(
+        item.id !== id
+      );
+    });
+    this.setState({items}); 
+  }
+
+  addItem = (item)=>{
+    item.id = Math.random();
+    let items = this.state.items;
+    items.push(item);
+    this.setState({items:items});
+  }
+
+  render(){
+
+    return (
+      <div className="App container">
+        <h1 className="text-center">TodoList App</h1>
+        <TodoItem items={this.state.items} deleteItem={this.deleteItem} />
+        <AddItem addItem={this.addItem}/>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/TodoItem/todoitem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const TodoItem = (props) => {
+    const  {items, deleteItem} = props;
+    let length = items.length;
+    const listItem = length ? ( items.map( item => {
+        return(
+            <div key={item.id}>
+                <span className='name'>{item.name}</span>
+                <span className='age'>{item.age}</span>
+                <span className='action icon' onClick={() => deleteItem(item.id)}>&times;</span>
+            </div>
+        )
+    })
+    ) :(
+    <p>
+        No Items to show
+    </p>
+    );
+    return(
+        <div className="ListItems">
+            <div>
+                <span className='name title'>Name</span>
+                <span className='age title'>Age</span>
+                <span className='action title'>Action</span>
+            </div>
+            {listItem}
+        </div>
+    )
+}
+
+export default TodoItem;
